f13e793dbd64032a737986e736d9199f26c90ad0
Sarun <sarunks0007@gmail.com>
2026-02-06 12:32:20 +0530

    product order and payments

--- storely/settings.py
+++ storely/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'accounts',
     'store',
     'carts',
+    'orders',
+    
 ]
 
 MIDDLEWARE = [
@@ -141,6 +143,6 @@ MESSAGE_TAGS = {
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sarunsharu17@gmail.com'
+EMAIL_HOST_USER = 'storedevops59@gmail.com'
 EMAIL_HOST_PASSWORD = 'your_app_password_here'
 EMAIL_USE_TLS = True
